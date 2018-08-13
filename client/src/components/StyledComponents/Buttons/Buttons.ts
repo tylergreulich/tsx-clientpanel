427319b9882c5b90332b5fcae3caf1c05070beee
tylergreulich <tgreul6@gmail.com>
2018-08-13 16:28:35 -0700

    FormButton Uppercase

--- client/src/components/StyledComponents/Buttons/Buttons.ts
+++ client/src/components/StyledComponents/Buttons/Buttons.ts
@@ -3,8 +3,11 @@ import { Button } from '@material-ui/core';
 
 interface FormButtonProps {
   wide?: boolean;
+  uppercase?: boolean;
 }
 
 export const FormButton = styled(Button)`
   width: ${(props: FormButtonProps) => (props.wide ? '12.5rem' : null)};
+  text-transform: ${(props: FormButtonProps) =>
+    props.uppercase ? 'uppercase' : null};
 `;
